1ac7d6a01755a9e25c30ddb10d73d6287273ba52
VISTALL <vistall.valeriy@gmail.com>
2016-05-08 13:16:03 +0300

    compilation

---
+++ src/consulo/xslt/debugger/breakpoint/XsltBreakpointTypeResolver.java
@@ -0,0 +1,48 @@
+package consulo.xslt.debugger.breakpoint;
+
+import org.intellij.lang.xpath.xslt.XsltSupport;
+import org.intellij.plugins.xsltDebugger.XsltBreakpointType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.mustbe.consulo.RequiredReadAction;
+import com.intellij.ide.highlighter.XmlFileType;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import com.intellij.xdebugger.breakpoints.XLineBreakpointType;
+import consulo.xdebugger.breakpoints.XLineBreakpointTypeResolver;
+
+/**
+ * @author VISTALL
+ * @since 5/8/2016
+ */
+public class XsltBreakpointTypeResolver implements XLineBreakpointTypeResolver
+{
+	@Nullable
+	@Override
+	@RequiredReadAction
+	public XLineBreakpointType<?> resolveBreakpointType(@NotNull Project project, @NotNull VirtualFile virtualFile, int line)
+	{
+		final Document document = FileDocumentManager.getInstance().getDocument(virtualFile);
+		if(document == null)
+		{
+			return null;
+		}
+
+		final PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
+		if(psiFile == null)
+		{
+			return null;
+		}
+		final FileType fileType = psiFile.getFileType();
+		if(fileType != XmlFileType.INSTANCE || !XsltSupport.isXsltFile(psiFile))
+		{
+			return null;
+		}
+		return XsltBreakpointType.getInstance();
+	}
+}
--- src/org/intellij/plugins/xsltDebugger/XsltBreakpointType.java
+++ src/org/intellij/plugins/xsltDebugger/XsltBreakpointType.java
@@ -2,18 +2,14 @@ package org.intellij.plugins.xsltDebugger;
 
 import javax.swing.Icon;
 
+import org.consulo.lombok.annotations.LazyInstance;
 import org.intellij.lang.xpath.xslt.XsltSupport;
 import org.intellij.lang.xpath.xslt.impl.XsltChecker;
 import org.intellij.plugins.xsltDebugger.impl.XsltDebuggerEditorsProvider;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.icons.AllIcons;
-import com.intellij.ide.highlighter.XmlFileType;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.xdebugger.XSourcePosition;
@@ -27,67 +23,68 @@ import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider;
 * User: sweinreuter
 * Date: 03.03.11
 */
-public class XsltBreakpointType extends XLineBreakpointType<XBreakpointProperties> {
+public class XsltBreakpointType extends XLineBreakpointType<XBreakpointProperties>
+{
+	@NotNull
+	@LazyInstance
+	public static XsltBreakpointType getInstance()
+	{
+		return EXTENSION_POINT_NAME.findExtension(XsltBreakpointType.class);
+	}
 
-  private final XsltDebuggerEditorsProvider myMyEditorsProvider1 = new XsltDebuggerEditorsProvider(XsltChecker.LanguageLevel.V1);
-  private final XsltDebuggerEditorsProvider myMyEditorsProvider2 = new XsltDebuggerEditorsProvider(XsltChecker.LanguageLevel.V2);
+	private final XsltDebuggerEditorsProvider myMyEditorsProvider1 = new XsltDebuggerEditorsProvider(XsltChecker.LanguageLevel.V1);
+	private final XsltDebuggerEditorsProvider myMyEditorsProvider2 = new XsltDebuggerEditorsProvider(XsltChecker.LanguageLevel.V2);
 
-  public XsltBreakpointType() {
-    super("xslt", "XSLT Breakpoints");
-  }
+	public XsltBreakpointType()
+	{
+		super("xslt-line-breakpoint-type", "XSLT Line Breakpoints");
+	}
 
-  @Override
-  public boolean canPutAt(@NotNull VirtualFile file, int line, @NotNull Project project) {
-    final Document document = FileDocumentManager.getInstance().getDocument(file);
-    if (document == null) return false;
+	@Override
+	public XDebuggerEditorsProvider getEditorsProvider(@NotNull XLineBreakpoint<XBreakpointProperties> breakpoint, @NotNull Project project)
+	{
+		final XSourcePosition position = breakpoint.getSourcePosition();
+		if(position == null)
+		{
+			return null;
+		}
 
-    final PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
-    if (psiFile == null) {
-      return false;
-    }
-    final FileType fileType = psiFile.getFileType();
-    if (fileType != XmlFileType.INSTANCE || !XsltSupport.isXsltFile(psiFile)) {
-      return false;
-    }
-    return true;
-  }
+		final PsiFile file = PsiManager.getInstance(project).findFile(position.getFile());
+		if(file == null)
+		{
+			return null;
+		}
 
-  @Override
-  public XDebuggerEditorsProvider getEditorsProvider(@NotNull XLineBreakpoint<XBreakpointProperties> breakpoint, @NotNull Project project) {
-    final XSourcePosition position = breakpoint.getSourcePosition();
-    if (position == null) {
-      return null;
-    }
+		final XsltChecker.LanguageLevel level = XsltSupport.getXsltLanguageLevel(file);
+		if(level == XsltChecker.LanguageLevel.V1)
+		{
+			return myMyEditorsProvider1;
+		}
+		else if(level == XsltChecker.LanguageLevel.V2)
+		{
+			return myMyEditorsProvider2;
+		}
 
-    final PsiFile file = PsiManager.getInstance(project).findFile(position.getFile());
-    if (file == null) {
-      return null;
-    }
+		return null;
+	}
 
-    final XsltChecker.LanguageLevel level = XsltSupport.getXsltLanguageLevel(file);
-    if (level == XsltChecker.LanguageLevel.V1) {
-      return myMyEditorsProvider1;
-    } else if (level == XsltChecker.LanguageLevel.V2) {
-      return myMyEditorsProvider2;
-    }
+	@NotNull
+	@Override
+	public Icon getEnabledIcon()
+	{
+		return XsltSupport.createXsltIcon(AllIcons.Debugger.Db_set_breakpoint);
+	}
 
-    return null;
-  }
+	@NotNull
+	@Override
+	public Icon getDisabledIcon()
+	{
+		return XsltSupport.createXsltIcon(AllIcons.Debugger.Db_disabled_breakpoint);
+	}
 
-  @NotNull
-  @Override
-  public Icon getEnabledIcon() {
-    return XsltSupport.createXsltIcon(AllIcons.Debugger.Db_set_breakpoint);
-  }
-
-  @NotNull
-  @Override
-  public Icon getDisabledIcon() {
-    return XsltSupport.createXsltIcon(AllIcons.Debugger.Db_disabled_breakpoint);
-  }
-
-  @Override
-  public XBreakpointProperties createBreakpointProperties(@NotNull VirtualFile file, int line) {
-    return null;
-  }
+	@Override
+	public XBreakpointProperties createBreakpointProperties(@NotNull VirtualFile file, int line)
+	{
+		return null;
+	}
 }
