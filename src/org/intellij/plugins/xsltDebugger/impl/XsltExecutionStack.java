d9d738a966e148a69c5bc3d973fd1f93a2f13570
VISTALL <vistall.valeriy@gmail.com>
2016-11-11 09:36:53 +0300

    compilation

--- src/org/intellij/plugins/xsltDebugger/impl/XsltExecutionStack.java
+++ src/org/intellij/plugins/xsltDebugger/impl/XsltExecutionStack.java
@@ -1,50 +1,64 @@
 package org.intellij.plugins.xsltDebugger.impl;
 
-import com.intellij.xdebugger.frame.XExecutionStack;
-import com.intellij.xdebugger.frame.XStackFrame;
-import org.intellij.plugins.xsltDebugger.VMPausedException;
-import org.intellij.plugins.xsltDebugger.XsltDebuggerSession;
-import org.intellij.plugins.xsltDebugger.rt.engine.Debugger;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class XsltExecutionStack extends XExecutionStack {
-  private final XsltStackFrame myTopFrame;
-  private final XsltDebuggerSession myDebuggerSession;
+import org.intellij.plugins.xsltDebugger.VMPausedException;
+import org.intellij.plugins.xsltDebugger.XsltDebuggerSession;
+import org.intellij.plugins.xsltDebugger.rt.engine.Debugger;
+import com.intellij.xdebugger.frame.XExecutionStack;
+import com.intellij.xdebugger.frame.XStackFrame;
 
-  public XsltExecutionStack(String name, Debugger.Frame topFrame, XsltDebuggerSession debuggerSession) {
-    super(name);
-    myDebuggerSession = debuggerSession;
-    myTopFrame = new XsltStackFrame(topFrame, myDebuggerSession);
-  }
+public class XsltExecutionStack extends XExecutionStack
+{
+	private final XsltStackFrame myTopFrame;
+	private final XsltDebuggerSession myDebuggerSession;
 
-  @Override
-  public XStackFrame getTopFrame() {
-    return myTopFrame;
-  }
+	public XsltExecutionStack(String name, Debugger.Frame topFrame, XsltDebuggerSession debuggerSession)
+	{
+		super(name);
+		myDebuggerSession = debuggerSession;
+		myTopFrame = new XsltStackFrame(topFrame, myDebuggerSession);
+	}
 
-  @Override
-  public void computeStackFrames(int firstFrameIndex, XStackFrameContainer container) {
-    try {
-      if (myDebuggerSession.getCurrentState() == Debugger.State.SUSPENDED) {
-        Debugger.Frame frame = myTopFrame.getFrame();
-        final List<XStackFrame> frames = new ArrayList<XStackFrame>();
-        while (frame != null) {
-          frame = frame.getPrevious();
-          if (frame != null) {
-            frames.add(new XsltStackFrame(frame, myDebuggerSession));
-          }
-        }
-        if (firstFrameIndex <= frames.size()) {
-          container.addStackFrames(frames.subList(firstFrameIndex - 1, frames.size()), true);
-        } else {
-          container.addStackFrames(Collections.<XStackFrame>emptyList(), true);
-        }
-      }
-    } catch (VMPausedException e) {
-      container.errorOccurred(VMPausedException.MESSAGE);
-    }
-  }
+	@Override
+	public XStackFrame getTopFrame()
+	{
+		return myTopFrame;
+	}
+
+	@Override
+	public void computeStackFrames(XStackFrameContainer container)
+	{
+		try
+		{
+			if(myDebuggerSession.getCurrentState() == Debugger.State.SUSPENDED)
+			{
+				Debugger.Frame frame = myTopFrame.getFrame();
+				final List<XStackFrame> frames = new ArrayList<XStackFrame>();
+				while(frame != null)
+				{
+					frame = frame.getPrevious();
+					if(frame != null)
+					{
+						frames.add(new XsltStackFrame(frame, myDebuggerSession));
+					}
+				}
+				int firstFrameIndex = 0;
+				if(firstFrameIndex <= frames.size())
+				{
+					container.addStackFrames(frames.subList(firstFrameIndex - 1, frames.size()), true);
+				}
+				else
+				{
+					container.addStackFrames(Collections.<XStackFrame>emptyList(), true);
+				}
+			}
+		}
+		catch(VMPausedException e)
+		{
+			container.errorOccurred(VMPausedException.MESSAGE);
+		}
+	}
 }
